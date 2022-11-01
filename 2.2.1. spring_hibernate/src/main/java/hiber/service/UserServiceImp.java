35ca59a0a36259796407d3054212c8494b9d929d
Art40000 <artemirii@gmail.com>
2022-11-01 14:45:10 +0300

    C3

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,11 +1,13 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Id;
 import java.util.List;
 
 @Service
@@ -24,6 +26,16 @@ public class UserServiceImp implements UserService {
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
+   }
+   @Transactional(readOnly = true)
+   @Override
+   public List<Car> listCars() {
+      return userDao.listCars();
+   }
+   @Transactional(readOnly = true)
+   @Override
+   public User getUserHavingCar(int series, String model) {
+      return  userDao.getUserHavingCar(series, model);
    }
 
 }
